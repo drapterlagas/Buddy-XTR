4bffb4a03b7337f1ebe97baa830c810f85932eb8
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-04-29 14:25:46 +0300

    Update config.cjs
    Signed-off-by: ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>

--- config.cjs
+++ config.cjs
@@ -101,7 +101,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Buddy;;;wdpX3JwZ#cxw3fhqYP5zT1dMayFediroDBmpUudiIg3r_keEH3EE",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
